170ad065e6206bc4be1e73a1e3cecfaf6298f575
aprilhoward <apehow111@gmail.com>
2020-10-21 13:01:19 -0400

    implemented binary search

--- src/Theater.java
+++ src/Theater.java
@@ -46,29 +46,6 @@ public class Theater {
             System.out.println("There is no seat" + seatNumber);
             return false;
         }
-//        Seat requestedSeat = new Seat(seatNumber);
-//        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
-//        if (foundSeat >= 0) {
-//            return seats.get(foundSeat).reserve();
-//        } else {
-//            System.out.println("There is no seat " + seatNumber);
-//            return false;
-//        }
-//        }
-//        for (Seat seat : seats) {
-//            System.out.print(".");
-//            if (seat.getSeatNumber().equals(seatNumber)) {
-//                requestedSeat = seat;
-//                break;
-//            }
-//        }
-//
-//        if(requestedSeat == null) {
-//            System.out.println("There is no seat " + seatNumber);
-//            return false;
-//        }
-//
-//        return requestedSeat.reserve();
 
     // for testing
     public boolean getSeat() {
